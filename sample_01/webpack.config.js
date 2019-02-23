206228d277a27c1b59f060e4d7c0c3f5b49b00fc
okumurakengo <okumurakengo1a@gmail.com>
2019-02-23 15:55:30 +0900

    sample_01 added

---
+++ sample_01/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    mode: 'development',
+    module: {
+        rules: [{
+            test: /\.pcss/,
+            use: [
+                'style-loader',
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        plugins: [
+                            require('postcss-nested')
+                        ],
+                    },
+                },
+            ],
+        }]
+    }
+};
